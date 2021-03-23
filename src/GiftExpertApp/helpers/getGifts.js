e44206dec08947249e05a7dfdfff03878a6c37de
fergim1 <fernando@movenow.com.ar>
2021-03-23 18:27:32 -0300

    .

--- src/GiftExpertApp/helpers/getGifts.js
+++ src/GiftExpertApp/helpers/getGifts.js
@@ -1,6 +1,6 @@
 
 const getGifts = async ( category ) => {
-    const url = `//api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=8kLrlo8YQge6rb3zk8T3IBBLzUVNhkmH`;
+    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=8kLrlo8YQge6rb3zk8T3IBBLzUVNhkmH`;
     const resp = await fetch ( url );
     const {data} = await resp.json()
 
